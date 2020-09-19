b4dad3e0ae7b23a761784c1ee1384e8d88f33930
demsasha4yt <demsasha4yt@yandex.ru>
2020-09-19 16:10:12 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,14 +6,14 @@ module.exports = {
                 provider: "generic",
                 url: "https://gitlab.com/api/v4/projects/13366947/jobs/artifacts/master/raw/build/?job=build"
             },
-            token: "3wgZABiFU7AGcKKUgpML"
+            token: "1"
         },
         develop: {
             feedUrl: {
                 provider: "generic",
                 url: "https://gitlab.com/api/v4/projects/13366947/jobs/artifacts/develop/raw/build/?job=build"
             },
-            token: "3wgZABiFU7AGcKKUgpML"
+            token: "1"
         }
     }
 };
